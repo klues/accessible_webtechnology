bc8008452af26c091471b2eb4952caabd63a55dc
Benjamin Klaus <benjamin@asterics-foundation.org>
2025-09-12 14:05:27 +0200

    added data binding example

--- lecture_2/example_solution/components/MessageList.js
+++ lecture_2/example_solution/components/MessageList.js
@@ -2,16 +2,24 @@ import Message from "./Message.js";
 
 const htmlTemplate = /*html*/`
 <ol>
-   <li>
+   <li v-for="message of messages">
       <Message/>
    </li>
-   <li>
-      <Message/>
-   </li>
+   <button @click="addMessage()">Add</button>
 </ol>
 `
 
 export default {
   template: htmlTemplate,
-  components: { Message }
+  components: { Message },
+  data() {
+   return {
+      messages: ["Text 1", "Text 2", "Text 3"]
+   }
+  },
+  methods: {
+   addMessage() {
+      this.messages.push("New Text");
+   }
+  }
 };
